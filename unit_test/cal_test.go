9654c0a5a84cdbdaf25183a04f1e057f43419c4c
Chudjane <scg.chudjane23@gmail.com>
2022-03-10 11:01:33 +0700

    add test

---
+++ unit_test/cal_test.go
@@ -0,0 +1,24 @@
+package unit_test
+
+import (
+	"testing"
+)
+
+// Add is our function that sums two integers
+func Add(x, y int) (res int) {
+	return x + y
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) (res int) {
+	return x - y
+}
+func TestAdd(t *testing.T) {
+
+	got := Add(4, 6)
+	want := 10
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
